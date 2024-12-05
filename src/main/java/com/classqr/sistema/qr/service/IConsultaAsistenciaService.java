33c6a1f96f73f368322d9f4e1bf746a531f1c9ef
MariaFernanda1818 <mariafvn0127@gmail.com>
2024-12-05 17:00:23 -0500

    Clases sobrantes

--- src/main/java/com/classqr/sistema/qr/service/IConsultaAsistenciaService.java
+++
@@ -1,9 +0,0 @@
-package com.classqr.sistema.qr.service;
-
-import com.classqr.sistema.commons.dto.RespuestaGeneralDTO;
-
-public interface IConsultaAsistenciaService {
-
-    RespuestaGeneralDTO consultarTodasAsistencias();
-
-}
